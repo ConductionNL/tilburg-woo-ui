81abc8ba19f8784845a90a1189fb4cd3adc8ac22
Joost Wiechers <joostert@gmail.com>
2024-06-03 15:11:39 +0200

    Fixed content page bug where new location will not be fetched.

--- src/components/tilburg-navigation/tilburg-navigation.js
+++ src/components/tilburg-navigation/tilburg-navigation.js
@@ -24,7 +24,7 @@ const TilburgNavigation = () => {
                         </Link>
                     </li>
                     <li>
-                        <Link>
+                        <Link to="/toegankelijkheid">
                             <VISUALS.LIST />
                             Onderwerpen
                         </Link>
--- src/views/ac-content/ac-content.js
+++ src/views/ac-content/ac-content.js
@@ -2,8 +2,10 @@ import { useEffect } from 'react';
 import { withStore } from '@stores';
 import { observer } from 'mobx-react-lite';
 import loadable from '@loadable/component';
+import { useLocation} from 'react-router-dom';
+
 import { Heading } from '@utrecht/component-library-react/dist/css-module';
-import TilburgSection from '@atoms/tilburg-section/tilburg-section';
+
 
 const TilburgSectionsHandler = loadable(() => import('@components/tilburg-sections-handler/tilburg-sections-handler'));
 const TilburgContainer = loadable(() => import('@atoms/tilburg-container/tilburg-container'));
@@ -12,11 +14,11 @@ const AcContent = ({ store: { pages } }) => {
 
     const { fetchPage, get_single } = pages;
 
-    const slug = document.location.pathname;
+    const location = useLocation();
 
     useEffect(() => {
-        fetchPage(slug)
-    }, []);
+        fetchPage(location?.pathname)
+    }, [location]);
 
     return (
         <TilburgContainer compact>
