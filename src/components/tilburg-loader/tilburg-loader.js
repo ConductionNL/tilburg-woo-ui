4154ea8811c3a2c8894bcad73ccf14b007021556
Joost Wiechers <joostert@gmail.com>
2024-06-03 16:26:31 +0200

    Added page loader

---
+++ src/components/tilburg-loader/tilburg-loader.js
@@ -0,0 +1,11 @@
+const TilburgLoader = () => {
+    return (
+        <div className="tilburg-loader tilburg-loader--primary">
+            <span className="tilburg-loader-dot"></span>
+            <span className="tilburg-loader-dot"></span>
+            <span className="tilburg-loader-dot"></span>
+        </div>
+    )
+}
+
+export default TilburgLoader
--- src/views/ac-content/ac-content.js
+++ src/views/ac-content/ac-content.js
@@ -1,24 +1,30 @@
 import { useEffect } from 'react';
 import { withStore } from '@stores';
 import { observer } from 'mobx-react-lite';
+import { useLocation } from 'react-router-dom';
 import loadable from '@loadable/component';
-import { useLocation} from 'react-router-dom';
 
 import { Heading } from '@utrecht/component-library-react/dist/css-module';
 
+const TilburgLoader = loadable(() => import('@components/tilburg-loader/tilburg-loader'));
 
 const TilburgSectionsHandler = loadable(() => import('@components/tilburg-sections-handler/tilburg-sections-handler'));
 const TilburgContainer = loadable(() => import('@atoms/tilburg-container/tilburg-container'));
 
 const AcContent = ({ store: { pages } }) => {
 
-    const { fetchPage, get_single } = pages;
+    const { fetchPage, get_single, loading, resetPage } = pages;
 
     const location = useLocation();
 
     useEffect(() => {
+        resetPage()
         fetchPage(location?.pathname)
     }, [location]);
+
+    if (loading.status) {
+        return <TilburgLoader />
+    }
 
     return (
         <TilburgContainer compact>
