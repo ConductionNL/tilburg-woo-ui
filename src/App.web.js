01292964e10ef81d9e7ba457922e318f310f72b1
Jorik Bosman <jorik@acato.nl>
2024-06-03 10:41:36 +0200

    [MISC] added missing domains to csp, fixed check on all_pages in App.web.js

--- src/App.web.js
+++ src/App.web.js
@@ -3,8 +3,7 @@ import { withStore } from '@stores';
 import { observer } from 'mobx-react-lite';
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
-import loadable from '@loadable/component'
-
+import loadable from '@loadable/component';
 
 // Imports => SCSS
 import '@styles/index.scss';
@@ -13,81 +12,81 @@ import '@styles/index.scss';
 import config from '@config';
 
 // Imports => Constants
-import {
-    DEFAULT_ROUTE,
-} from '@constants';
+import { DEFAULT_ROUTE } from '@constants';
 
 // Imports => Utilities
-import { AcHome } from '@views'
+import { AcHome } from '@views';
 import AcContent from '@views/ac-content/ac-content';
 
 // Imports => Molecules
-const TilburgHeader = loadable(() => import('@components/tilburg-header/tilburg-header'));
-const TilburgFooter = loadable(() => import('@components/tilburg-footer/tilburg-footer'));
+const TilburgHeader = loadable(() =>
+  import('@components/tilburg-header/tilburg-header')
+);
+const TilburgFooter = loadable(() =>
+  import('@components/tilburg-footer/tilburg-footer')
+);
 
 // Imports => Atoms
 
 const _CLASSES = {
-    ROOT: 'ac-root',
-    MAIN: 'ac-app',
-    ROUTE: {
-        SECTION: 'ac-route__section',
-        HIDDEN: 'ac-route__section--hidden',
-    },
+  ROOT: 'ac-root',
+  MAIN: 'ac-app',
+  ROUTE: {
+    SECTION: 'ac-route__section',
+    HIDDEN: 'ac-route__section--hidden',
+  },
 };
 
 const App = ({ store }) => {
+  const { fetchPages, all_pages } = store.pages;
 
-    const { fetchPages, all_pages } = store.pages;
+  useEffect(() => {
+    fetchPages();
+  }, []);
 
-    useEffect(() => {
-        fetchPages()
-    }, []);
+  useEffect(() => {
+    console.log(all_pages);
+    all_pages?.map((page) => {
+      console.log(page.slug);
+    });
+  }, [all_pages]);
 
-    useEffect(() => {
-        console.log(all_pages)
-        all_pages?.map((page) => {
-            console.log(page.slug)
-        })
-    }, [all_pages]);
-
-    const getView = (page) => {
-        return page.slug === 'home'
-            ? <AcHome store={store} />
-            : <AcContent store={store} />
-    }
-
-    if (all_pages.length <= 0) {
-        return null
-    }
-
-    return (
-        <div class="tilburg-theme">
-            <TilburgHeader />
-
-            <main id="main">
-                <Routes>
-                    {all_pages?.map((page) =>
-                        <Route
-                            key={`route-${page.id}`}
-                            path={page.slug}
-                            element={
-                                getView(page)
-                            } />,
-                    )}
-                    <Route
-                        key={`default-route-${DEFAULT_ROUTE.id}`}
-                        path={'*'}
-                        element={
-                            <AcHome store={store} />
-                        }
-                    />
-                </Routes>
-            </main>
-
-            <TilburgFooter />
-        </div>
+  const getView = (page) => {
+    return page.slug === 'home' ? (
+      <AcHome store={store} />
+    ) : (
+      <AcContent store={store} />
     );
+  };
+
+  if (!all_pages?.length) {
+    return null;
+  }
+
+  return (
+    <div class='tilburg-theme'>
+      <TilburgHeader />
+
+      <main id='main'>
+        <Routes>
+          {all_pages?.map((page) => (
+            <Route
+              key={`route-${page.id}`}
+              path={page.slug}
+              element={getView(page)}
+            />
+          ))}
+          <Route
+            key={`default-route-${DEFAULT_ROUTE.id}`}
+            path={'*'}
+            element={<AcHome store={store} />}
+          />
+        </Routes>
+      </main>
+
+      <TilburgFooter />
+    </div>
+  );
 };
 
 export default withStore(observer(App));
