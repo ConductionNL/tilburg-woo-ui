673f87f53623d145135b24434896e6844483b075
Jorik Bosman <jorik@acato.nl>
2024-06-04 10:26:11 +0200

    [MISC] code cleanup

--- src/stores/faqs.store.js
+++ src/stores/faqs.store.js
@@ -1,53 +1,53 @@
 // Imports => MOBX
-import {observable, computed, makeObservable, action} from 'mobx';
+import { observable, computed, makeObservable, action } from 'mobx';
 
 // Imports => Utilities
-import {AcSanitizeHtml} from "@src/utilities";
+import { AcSanitizeHtml } from '@src/utilities';
 
 let app = {};
 
 export class FaqsStore {
-    constructor(store) {
-        makeObservable(this);
-        app.store = store;
-    }
+  constructor(store) {
+    makeObservable(this);
+    app.store = store;
+  }
 
-    @observable
-    items = [];
+  @observable
+  items = [];
 
-    @observable
-    loading = {
-        status: false,
-        message: undefined,
-    };
+  @observable
+  loading = {
+    status: false,
+    message: undefined,
+  };
 
-    @computed
-    get is_loading() {
-        return this.loading.status;
-    }
+  @computed
+  get is_loading() {
+    return this.loading.status;
+  }
 
-    @computed
-    get all_faqs() {
-        return this.items.map((item) => ({
-            label: item.question,
-            body: AcSanitizeHtml(item.answer),
-        }))
-    }
+  @computed
+  get all_faqs() {
+    return this.items?.map((item) => ({
+      label: item.question,
+      body: AcSanitizeHtml(item.answer),
+    }));
+  }
 
-    @action
-    fetchFaqs = async () => {
-        this.loading.status = true
+  @action
+  fetchFaqs = async () => {
+    this.loading.status = true;
 
-        app.store.api.faqs.list()
-            .then((response) => {
-                this.items = response.data
-            })
-            .catch(e => console.error(e))
-            .finally(() => {
-                this.loading.status = false
-            })
-    }
-
+    app.store.api.faqs
+      .list()
+      .then((response) => {
+        this.items = response.data;
+      })
+      .catch((e) => console.error(e))
+      .finally(() => {
+        this.loading.status = false;
+      });
+  };
 }
 
 export default FaqsStore;
