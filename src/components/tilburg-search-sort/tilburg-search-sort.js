54baeb963c80a0808a64299dac92a0b6bcaf2ad1
Mark Teekman <mark.teekman@gmail.com>
2024-07-04 14:54:55 +0200

    Updates to search page. TILBURG-18

--- src/components/tilburg-search-sort/tilburg-search-sort.js
+++ src/components/tilburg-search-sort/tilburg-search-sort.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { observer } from 'mobx-react-lite';
 
 import { withStore } from '@stores';
@@ -12,35 +11,19 @@ import {
   Select,
   SelectOption,
 } from '@utrecht/component-library-react/dist/css-module';
-import { Form } from 'react-router-dom';
 
 const TilburgSearchSort = ({ store: { documents } }) => {
-  const { setQueryDate } = documents;
-
-  const setDate = (key, value) => {
-    if (!AcValidateDate(value)) {
-      setQueryDate(key, null);
-      return;
-    }
-    setQueryDate(key, value);
-  };
-
   return (
-    <TilburgFlex className='tilburg-search-filters__sort'>
-      <FormField>
-        <FormLabel>
-          <Heading level={4}>Sorteren</Heading>
-        </FormLabel>
-        <Select
-          defaultValue={documents.search_query['_sort']}
-          onChange={(e) => documents.setQuery('_sort', e.target.value)}
-        >
-          <SelectOption value='relevance'>Meest relevant</SelectOption>
-          <SelectOption value='publicatiedatum'>Datum - oud naar nieuw</SelectOption>
-          <SelectOption value='publicatiedatum'>Datum - nieuw naar oud</SelectOption>
-        </Select>
-      </FormField>
-    </TilburgFlex>
+    <FormField type='select'>
+      <FormLabel>
+        <Heading level={4}>Sorteren</Heading>
+      </FormLabel>
+      <Select>
+        <SelectOption value='relevance'>Meest relevant</SelectOption>
+        <SelectOption value='publicatiedatum'>Datum - oud naar nieuw</SelectOption>
+        <SelectOption value='publicatiedatum'>Datum - nieuw naar oud</SelectOption>
+      </Select>
+    </FormField>
   );
 };
 
