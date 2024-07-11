735313b007281a9832dca0664d0f211fbc26c91c
Joost Wiechers <joostert@gmail.com>
2024-07-11 10:14:13 +0200

    Code improvement

--- src/components/tilburg-search-date/tilburg-search-date.js
+++ src/components/tilburg-search-date/tilburg-search-date.js
@@ -7,7 +7,7 @@ import { AcValidateDate } from '@utils';
 import { TilburgFormField } from '@molecules';
 
 const TilburgSearchDate = ({ store: { documents } }) => {
-  const { setQueryDate } = documents;
+  const { setQueryDate, search_query } = documents;
 
   const setDate = (key, value) => {
     if (!AcValidateDate(value)) {
@@ -21,14 +21,14 @@ const TilburgSearchDate = ({ store: { documents } }) => {
     <TilburgFlex column spacing='sm' className='tilburg-search-filters__date'>
       <TilburgFormField
         id={'date_after'}
-        defaultValue={documents.search_query['publicationDate[after]']}
+        defaultValue={search_query['publicationDate[after]']}
         label='Datum vanaf (dd-mm-yyyy)'
         placeholder='dd-mm-yyyy'
         onBlur={(value) => setDate('after', value)}
       />
       <TilburgFormField
         id={'date_before'}
-        defaultValue={documents.search_query['publicationDate[before]']}
+        defaultValue={search_query['publicationDate[before]']}
         label='Datum tot en met (dd-mm-yyyy)'
         placeholder='dd-mm-yyyy'
         onBlur={(value) => setDate('before', value)}
