d70906520049c89cb04b65dce67b6959e98750d8
Mark Teekman <mark.teekman@gmail.com>
2024-07-29 10:52:51 +0200

    Remove dummy data

--- src/views/ac-home/ac-home.js
+++ src/views/ac-home/ac-home.js
@@ -2,32 +2,8 @@ import { withStore } from '@stores';
 import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
 
-import { AcAbout, AcHero, AcIntro, AcLoader, AcSubjects } from '@components';
+import { AcAbout, AcHero, AcIntro, AcLoader } from '@components';
 import { AcRemoveParagraphTags, AcRemoveTags, AcSanitizeHtml } from '@utils';
-
-const subjectsDummyData = [
-  {
-    image: '/card-placeholder-1.png',
-    title: 'Campus Wijkevoort',
-    paragraph:
-      'Op de campus gaan bedrijven, onderwijs – en onderzoeksinstellingen ook samen innoveren en medewerkers opleiden.',
-    linkTitle: 'Bekijk 209 documenten',
-  },
-  {
-    image: '/card-placeholder-2.png',
-    title: 'Evenementen in Ac',
-    paragraph:
-      'Ac is genomineerd voor ‘Evenementenstad van het jaar’. Bekijk alle publicaties over dit onderwerp.',
-    linkTitle: 'Bekijk 511 documenten',
-  },
-  {
-    image: '/card-placeholder-3.png',
-    title: 'Duurzaamheid',
-    paragraph:
-      'Ac is onderweg naar een duurzame stad. Bekijk alle beslissingen omtrent het duurzame stadsbeleid.',
-    linkTitle: 'Bekijk 3.040 documenten',
-  },
-];
 
 const AcHome = ({ store: { pages } }) => {
   const { fetchPage, resetPage, get_single } = pages;
