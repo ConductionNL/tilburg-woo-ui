5fcc49c5bc10d94f7f9309fd6d641a69a4cb875a
Mark Teekman <128050096+markacato@users.noreply.github.com>
2025-01-09 16:11:36 +0100

    Add new search filter custom component. TILBURG-335

---
+++ src/components/ac-search-filter/ac-search-filter.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { VISUALS } from '@constants';
+import {
+  FormLabel,
+  Textbox,
+  PrimaryActionButton,
+  SecondaryActionButton,
+} from '@utrecht/component-library-react/dist/css-module';
+import { AcFlex } from '@atoms';
+
+const AcSearchFilter = ({
+  placeholder = 'Waar ben je naar op zoek?',
+  onSearch,
+  initialValue = '',
+  label = 'Zoek in begrippen',
+}) => {
+  const [searchTerm, setSearchTerm] = useState(initialValue);
+  const [currentSearchTerm, setCurrentSearchTerm] = useState('');
+  const [resultCount, setResultCount] = useState(null);
+
+  const getResultText = (count) => {
+    return count === 1 ? 'resultaat' : 'resultaten';
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCurrentSearchTerm(searchTerm);
+    const count = onSearch(searchTerm);
+    setResultCount(count);
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const clearSearch = () => {
+    setSearchTerm('');
+    setCurrentSearchTerm('');
+    setResultCount(null);
+    onSearch('');
+  };
+
+  return (
+    <div className='ac-search-filter'>
+      <form onSubmit={handleSubmit}>
+        <FormLabel htmlFor='ac-search-filter-input'>{label}</FormLabel>
+        <AcFlex alignItems='center' spacing='xs'>
+          <Textbox
+            id='ac-search-filter-input'
+            type='search'
+            value={searchTerm}
+            onChange={handleChange}
+            placeholder={placeholder}
+          />
+          <PrimaryActionButton type='submit'>
+            <VISUALS.SEARCH />
+            <span className='sr-only'>Zoek</span>
+          </PrimaryActionButton>
+        </AcFlex>
+      </form>
+
+      {currentSearchTerm && (
+        <div className='ac-search-filter__results'>
+          Gezocht op: <strong>{currentSearchTerm}</strong> ({resultCount}{' '}
+          {getResultText(resultCount)})
+          <br />
+          <button onClick={clearSearch}>Wis zoekopdracht</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AcSearchFilter;
