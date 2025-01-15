a693f911aea62f7735e28edbb53bf6cc8745ef04
Remko <remko@conduction.nl>
2025-01-15 13:16:25 +0100

    Updated footer

--- src/components/ac-footer/ac-footer.js
+++ src/components/ac-footer/ac-footer.js
@@ -7,10 +7,55 @@ import {
   VNG_FOOTER_ITEMS_SITEMAP,
   VNG_FOOTER_ITEMS_INFORMATIE,
   VNG_FOOTER_ITEMS_BEDRIJVEN,
+  DIMPACT_FOOTER_ITEMS_WHAT_WE_DO,
+  DIMPACT_FOOTER_ITEMS_WHO_WE_ARE,
+  DIMPACT_FOOTER_ITEMS_INFORMATION,
 } from '@constants/routes.constants';
 import { Link } from 'react-router-dom';
 
 const AcFooter = () => {
+  const getFooterItems = () => {
+    const hostname = window.location.hostname;
+
+    switch (hostname) {
+      case 'vng.opencatalogi.nl':
+        return [
+          VNG_FOOTER_ITEMS_SITEMAP,
+          VNG_FOOTER_ITEMS_INFORMATIE,
+          VNG_FOOTER_ITEMS_BEDRIJVEN,
+        ];
+      case 'open-tilburg.accept.commonground.nu':
+        return [
+          VNG_FOOTER_ITEMS_SITEMAP,
+          VNG_FOOTER_ITEMS_INFORMATIE,
+          VNG_FOOTER_ITEMS_BEDRIJVEN,
+        ];
+      case 'open-dimpact.accept.commonground.nu':
+        return [
+          DIMPACT_FOOTER_ITEMS_WHAT_WE_DO,
+          DIMPACT_FOOTER_ITEMS_WHO_WE_ARE,
+          DIMPACT_FOOTER_ITEMS_INFORMATION,
+        ];
+      case 'open-rotterdam.accept.commonground.nu':
+        return [
+          VNG_FOOTER_ITEMS_SITEMAP,
+          VNG_FOOTER_ITEMS_INFORMATIE,
+          VNG_FOOTER_ITEMS_BEDRIJVEN,
+        ];
+      case 'localhost':
+        return [
+          DIMPACT_FOOTER_ITEMS_WHAT_WE_DO,
+          DIMPACT_FOOTER_ITEMS_WHO_WE_ARE,
+          DIMPACT_FOOTER_ITEMS_INFORMATION,
+        ];
+      default:
+        return [
+          VNG_FOOTER_ITEMS_SITEMAP,
+          VNG_FOOTER_ITEMS_INFORMATIE,
+          VNG_FOOTER_ITEMS_BEDRIJVEN,
+        ];
+    }
+  };
 
   return (
     <footer className='ac-footer'>
@@ -20,39 +65,67 @@ const AcFooter = () => {
       >
         {AcCheckIfSpecificHostname() ? (
           <>
-            <nav className='ac-footer__links' aria-label='Footer menu 1'>
-              {VNG_FOOTER_ITEMS_SITEMAP.map((item, index) =>
-                item.label === 'FAQ' ? (
-                  <Link className='ac-footer__link' to={item.href}>
-                    {item.label}
-                  </Link>
-                ) : (
-                  <a href={item.href} className='ac-footer__link'>
-                    {item.label}
-                    <span className='sr-only'>Opent in een nieuw tabblad</span>
-                    <VISUALS.EXTERNAL_LINK />
-                  </a>
-                )
-              )}
-            </nav>
-            <nav className='ac-footer__links' aria-label='Footer menu 2'>
-              {VNG_FOOTER_ITEMS_INFORMATIE.map((item, index) => (
-                <a href={item.href} target='_blank' className='ac-footer__link'>
-                  {item.label}
-                  <span className='sr-only'>Opent in een nieuw tabblad</span>
-                  <VISUALS.EXTERNAL_LINK />
-                </a>
-              ))}
-            </nav>
-            <nav className='ac-footer__links' aria-label='Footer menu 3'>
-              {VNG_FOOTER_ITEMS_BEDRIJVEN.map((item, index) => (
-                <a href={item.href} target='_blank' className='ac-footer__link'>
-                  {item.label}
-                  <span className='sr-only'>Opent in een nieuw tabblad</span>
-                  <VISUALS.EXTERNAL_LINK />
-                </a>
-              ))}
-            </nav>
+            {getFooterItems().map((items, index) => (
+              <nav
+                className='ac-footer__links'
+                key={`footer-menu-${index + 1}`}
+                aria-label={`Footer menu ${index + 1}`}
+              >
+                {items.map((item, index) =>
+                  item.href ? (
+                    item.href.includes('http' || 'https') ? (
+                      <a
+                        href={item.href}
+                        target='_blank'
+                        className='ac-footer__link'
+                      >
+                        {item.iconLocation && item.iconLocation === 'left' ? (
+                          <>
+                            {item.icon ? (
+                              <item.icon className='ac-footer__link-icon' />
+                            ) : (
+                              <VISUALS.EXTERNAL_LINK className='ac-footer__link-icon' />
+                            )}
+                            {item.label}
+                            <span className='sr-only'>
+                              Opent in een nieuw tabblad
+                            </span>
+                          </>
+                        ) : (
+                          <>
+                            {item.label}
+                            <span className='sr-only'>
+                              Opent in een nieuw tabblad
+                            </span>
+                            <VISUALS.EXTERNAL_LINK className='ac-footer__link-icon' />
+                          </>
+                        )}
+                      </a>
+                    ) : (
+                      <Link className='ac-footer__link' to={item.href}>
+                        {item.icon && item.iconLocation === 'left' && (
+                          <item.icon className='ac-footer__link-icon' />
+                        )}
+                        {item.label}
+                        {item.icon && item.iconLocation !== 'left' && (
+                          <item.icon className='ac-footer__link-icon' />
+                        )}
+                      </Link>
+                    )
+                  ) : (
+                    <div className='ac-footer__link'>
+                      {item.icon && item.iconLocation === 'left' && (
+                        <item.icon className='ac-footer__link-icon' />
+                      )}
+                      {item.label}
+                      {item.icon && item.iconLocation !== 'left' && (
+                        <item.icon className='ac-footer__link-icon' />
+                      )}
+                    </div>
+                  )
+                )}
+              </nav>
+            ))}
           </>
         ) : (
           <>
