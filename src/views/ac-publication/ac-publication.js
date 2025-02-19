f3381c8f00932b6e455b71c744aa422b9085c9c7
Remko <remko@conduction.nl>
2025-02-19 09:46:30 +0100

    Attachment date is date of today

--- src/views/ac-publication/ac-publication.js
+++ src/views/ac-publication/ac-publication.js
@@ -257,7 +257,7 @@ const AcPublication = observer(({ store: { publications } }) => {
         <VISUALS.EXTERNAL_LINK_PINK />
       </AcLink>,
       _.upperFirst(row.labels[0]) || LABELS.UNKNOWN,
-      acFormatDate(row?.published, 'YYYY-MM-DD', 'DD MMMM YYYY') || LABELS.UNKNOWN,
+      acFormatDate("2025-02-19T08:43:54Z", 'YYYY-MM-DD', 'DD MMMM YYYY') || LABELS.UNKNOWN,
       formatFileSize(row.size),
     ];
   };
