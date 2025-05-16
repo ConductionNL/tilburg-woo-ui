13bd298571a604976f2e5bfe420d0e5fc3e2b08c
Thijn <thijn@conduction.nl>
2025-05-16 17:07:21 +0200

    WIP - download functionality

--- src/hooks/con-nextcloud-requests.js
+++ src/hooks/con-nextcloud-requests.js
@@ -1,4 +1,5 @@
 import { getCookie } from '@src/utilities';
+import { BASE_URL } from '@src/views/ac-beheer/ac-beheer';
 import { useNavigate } from 'react-router';
 
 export default function useNextcloudRequests() {
@@ -43,5 +44,63 @@ export default function useNextcloudRequests() {
     return response;
   };
 
-  return { makeRequest };
+  const makeDownloadRequest = async (
+    url,
+    queryParams,
+    fetchOptions = {},
+    redirectUrl,
+    _filename
+  ) => {
+    const response = await makeRequest(url, queryParams, fetchOptions, redirectUrl);
+
+    if (!response.ok) {
+      console.error('Failed to download file', response.status, response.statusText);
+      return;
+    }
+
+    // Get filename from Content-Disposition header or fallback to URL
+    const contentDisposition = response.headers.get('content-disposition');
+    const filename =
+      contentDisposition?.split('filename=')[1]?.replace(/["']/g, '') ||
+      _filename ||
+      url.split('/').pop();
+
+    const blob = await response.blob();
+    const downloadUrl = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = downloadUrl;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(downloadUrl);
+  };
+
+  /**
+   * Download an object from the Nextcloud API
+   * @param {string} register - The register to download
+   * @param {string} schema - The schema to download
+   * @param {'csv' | 'excel'} type - The type to download (csv, excel)
+   */
+  const downloadObjectList = async (
+    register,
+    schema,
+    type = 'csv',
+    redirectUrl = '/beheer'
+  ) => {
+    const url = `${BASE_URL}/apps/${register}/${schema}/export?type=${type}`;
+    const extension = type === 'excel' ? 'xlsx' : 'csv';
+    const currentDate = new Date().toISOString().split('T')[0];
+    const filename = `${schema}-${currentDate}.${extension}`;
+
+    await makeDownloadRequest(
+      url,
+      null,
+      { method: 'GET', headers: { 'Content-Type': '*/*' } },
+      redirectUrl,
+      filename
+    );
+  };
+
+  return { makeRequest, makeDownloadRequest, downloadObjectList };
 }
