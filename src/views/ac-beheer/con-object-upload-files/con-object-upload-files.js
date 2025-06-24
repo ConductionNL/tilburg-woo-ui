84c5b7809a2a450c191df4710daf832f309979cd
Remko <remko@conduction.nl>
2025-06-24 16:07:05 +0200

    updated icon

--- src/views/ac-beheer/con-object-upload-files/con-object-upload-files.js
+++ src/views/ac-beheer/con-object-upload-files/con-object-upload-files.js
@@ -209,7 +209,20 @@ const ConObjectUploadFiles = ({ register, schema, id, onSuccess = () => {} }) =>
               label: 'Titel',
               key: '',
               customContent: (row) => {
-                return <div>{row.title || '-'}</div>;
+                return (
+                  <div className='ac-beheer-organisaties-name-container'>
+                    <div className='ac-beheer-organisaties-name-container__icon'>
+                      {row.published ? (
+                        <VISUALS.CIRCLE_CHECK className='ac-beheer-publish-icon__check' />
+                      ) : (
+                        <VISUALS.CIRCLE_EXCLAMATION className='ac-beheer-publish-icon__exclamation' />
+                      )}
+                    </div>
+                    <div className='ac-beheer-organisaties-name-container__name'>
+                      {row.title || '-'}
+                    </div>
+                  </div>
+                );
               },
               sortComparator: (a, b, direction) => {
                 if (direction === null) return 0;
@@ -295,33 +308,7 @@ const ConObjectUploadFiles = ({ register, schema, id, onSuccess = () => {} }) =>
                 return direction ? priorityA - priorityB : priorityB - priorityA;
               },
             },
-            {
-              id: 'published',
-              label: 'Is gepubliceerd',
-              key: '',
-              customContent: (row) => {
-                if (row.isNew) return null;
-                if (row.published) return <VISUALS.CHECK style={{ color: 'green' }} />;
-                return <VISUALS.CIRCLE_EXCLAMATION style={{ color: 'orange' }} />;
-              },
-              sortComparator: (a, b, direction) => {
-                if (direction === null) return 0;
 
-                const publishedA = a.published;
-                const publishedB = b.published;
-
-                // Handle cases where one or both values are null
-                if (publishedA === null && publishedB === null) return 0;
-                if (publishedA === null) return direction ? 1 : -1;
-                if (publishedB === null) return direction ? -1 : 1;
-
-                // Compare dates
-                const dateA = new Date(publishedA);
-                const dateB = new Date(publishedB);
-
-                return direction ? dateA - dateB : dateB - dateA;
-              },
-            },
             {
               id: 'actions',
               label: 'Acties',
