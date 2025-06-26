41a005fbf07bf58addc09efef50b97d8e90467be
Remko <remko@conduction.nl>
2025-06-26 17:57:11 +0200

    added loading state to filters

--- src/molecules/con-facets-filters/con-facets-filters.js
+++ src/molecules/con-facets-filters/con-facets-filters.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { AcCheckbox } from '@molecules';
 import { LABELS } from '@constants';
 import { withStore } from '@stores';
+import { AcLoader } from '@components';
 
 import { Heading } from '@utrecht/component-library-react/dist/css-module';
 import { AcFlex } from '@atoms';
@@ -11,6 +12,7 @@ import _ from 'lodash';
 
 const ConFacetsFilters = ({ store: { publications } }) => {
   const [facets, setFacets] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const { theme_checked, toggleSearchArrayValue } = publications;
 
   // Function to build facetsQueries from available facets
@@ -20,7 +22,10 @@ const ConFacetsFilters = ({ store: { publications } }) => {
     // Handle @self facets
     if (availableFacets['@self']) {
       Object.entries(availableFacets['@self']).forEach(([key, config]) => {
-        if (key !== 'catalog' && config.facet_types && config.facet_types.includes('terms')) {
+        if (
+          config.facet_types &&
+          config.facet_types.includes('terms')
+        ) {
           queries.push([['@self', key], 'terms']);
         }
       });
@@ -48,12 +53,13 @@ const ConFacetsFilters = ({ store: { publications } }) => {
     };
 
     const fetchFacets = async () => {
-      const availableFacets = await fetchAvailableFacets();
-
-      // Build dynamic facetsQueries
-      const dynamicFacetsQueries = buildFacetsQueries(availableFacets);
-
+      setIsLoading(true);
       try {
+        const availableFacets = await fetchAvailableFacets();
+
+        // Build dynamic facetsQueries
+        const dynamicFacetsQueries = buildFacetsQueries(availableFacets);
+
         const queryParams = dynamicFacetsQueries
           .map(([key, value]) => {
             if (Array.isArray(key)) {
@@ -78,11 +84,17 @@ const ConFacetsFilters = ({ store: { publications } }) => {
         setFacets(data.facets);
       } catch (error) {
         console.error('Error fetching facets:', error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchFacets();
   }, []);
+
+  if (isLoading) {
+    return <AcLoader style={{ height: '200px' }} />;
+  }
 
   return (
     <>
@@ -91,47 +103,51 @@ const ConFacetsFilters = ({ store: { publications } }) => {
         Object.entries(facets).map(([key, value]) => {
           return key === '@self' ? (
             <>
-              {console.log({ value })}
-              {Object.entries(value).map(([_key, _value]) => (
-                <AcFlex
-                  key={`${key}-${_key}`}
-                  column
-                  spacing='xs'
-                  className='ac-search-filters__subjects'
-                >
-                  <Heading level={4}>{_.upperFirst(_key)}</Heading>
-                  {_value.buckets.map((bucket) => (
-                    <AcCheckbox
-                      key={bucket.key}
-                      label={`${bucket.label ?? bucket.key} (${bucket.results})`}
-                      value={bucket.key}
-                      checked={theme_checked(bucket.key)}
-                      onChange={() =>
-                        toggleSearchArrayValue(`${key}[${_key}]`, bucket.key)
-                      }
-                    />
-                  ))}
-                </AcFlex>
-              ))}
+              {Object.entries(value).map(
+                ([_key, _value]) =>
+                  _value.buckets.length > 1 && (
+                    <AcFlex
+                      key={`${key}-${_key}`}
+                      column
+                      spacing='xs'
+                      className='ac-search-filters__subjects'
+                    >
+                      <Heading level={4}>{_.upperFirst(_key)}</Heading>
+                      {_value.buckets.map((bucket) => (
+                        <AcCheckbox
+                          key={bucket.key}
+                          label={`${bucket.label ?? bucket.key} (${bucket.results})`}
+                          value={bucket.key}
+                          checked={theme_checked(bucket.key)}
+                          onChange={() =>
+                            toggleSearchArrayValue(`${key}[${_key}]`, bucket.key)
+                          }
+                        />
+                      ))}
+                    </AcFlex>
+                  )
+              )}
             </>
           ) : (
-            <AcFlex
-              key={key}
-              column
-              spacing='xs'
-              className='ac-search-filters__subjects'
-            >
-              <Heading level={4}>{_.upperFirst(key)}</Heading>
-              {value.buckets.map((value) => (
-                <AcCheckbox
-                  key={value.key}
-                  label={`${value.label ?? value.key} (${value.results})`}
-                  value={value.key}
-                  checked={theme_checked(value.key)}
-                  onChange={() => toggleSearchArrayValue(key, value.key)}
-                />
-              ))}
-            </AcFlex>
+            value.buckets.length > 1 && (
+              <AcFlex
+                key={key}
+                column
+                spacing='xs'
+                className='ac-search-filters__subjects'
+              >
+                <Heading level={4}>{_.upperFirst(key)}</Heading>
+                {value.buckets.map((value) => (
+                  <AcCheckbox
+                    key={value.key}
+                    label={`${value.label ?? value.key} (${value.results})`}
+                    value={value.key}
+                    checked={theme_checked(value.key)}
+                    onChange={() => toggleSearchArrayValue(key, value.key)}
+                  />
+                ))}
+              </AcFlex>
+            )
           );
         })}
     </>
