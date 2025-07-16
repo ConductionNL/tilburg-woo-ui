f181425a64948863382b6a4b88900b7d09a16aa4
Thijn <thijn@conduction.nl>
2025-07-16 14:18:22 +0200

    removed add option from details

--- src/views/ac-beheer/ac-contactpersonen/pages/ac-contactpersonen-details.js
+++ src/views/ac-beheer/ac-contactpersonen/pages/ac-contactpersonen-details.js
@@ -141,9 +141,6 @@ const AcBeheerContactpersoonDetails = ({ id }) => {
                     </ConActionMenu.Trigger>
 
                     <ConActionMenu.Menu position='right'>
-                      <ConActionMenu.Button icon={<VISUALS.PLUS />}>
-                        Toevoegen
-                      </ConActionMenu.Button>
                       <ConActionMenu.Button
                         icon={<VISUALS.PENCIL />}
                         onClick={() => setOpenModal('edit')}
