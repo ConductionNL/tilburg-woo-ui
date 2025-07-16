a8f377a95a56e33e2897f223db99aaaa3375b016
Thijn <thijn@conduction.nl>
2025-07-16 14:16:35 +0200

    publish modal text is improved

--- src/views/ac-beheer/ac-contactpersonen/modals/ac-publish-depublish-contactpersoon.js
+++ src/views/ac-beheer/ac-contactpersonen/modals/ac-publish-depublish-contactpersoon.js
@@ -120,8 +120,9 @@ const AcPublishDepublishContactpersoonModal = ({
     >
       <AcFlex column spacing='sm'>
         {error && <div style={errorStyle}>{error}</div>}
-        Weet je zeker dat je deze contactpersoon wilt{' '}
-        {publish ? 'publiceren' : 'depubliceren'}?
+        Weet je zeker dat je de volgende gebruiker wilt{' '}
+        {publish ? 'publiceren' : 'depubliceren'}? Hiermee wordt deze gebruiker{' '}
+        {!publish && 'niet meer'} zichtbaar voor anderen.
         <Paragraph>{contactpersoon?.username ?? contactpersoon?.id}</Paragraph>
       </AcFlex>
     </AcModal>
