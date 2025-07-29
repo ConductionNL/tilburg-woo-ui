e37cc4f84f31b08d9c1981d0495f14442e39724f
Thijn <thijn@conduction.nl>
2025-07-29 16:04:25 +0200

    more import fixes

--- src/molecules/con-facets-filters/con-facets-filters.js
+++ src/molecules/con-facets-filters/con-facets-filters.js
@@ -1,8 +1,8 @@
-import { observer } from 'mobx-react-lite';
+// eslint-disable-next-line import/no-unresolved
 import { useEffect, useState } from 'react';
+import { observer } from 'mobx-react-lite';
 import { buildPublicationsSearchQuery } from '@stores/publications.store';
 import { AcCheckbox } from '@molecules';
-import { LABELS } from '@constants';
 import { withStore } from '@stores';
 import { AcLoader } from '@components';
 import { AcBuildURLSearchParams } from '@utils';
