75d132bdd07ebe3e38f45ae2bb34cee67a882f7b
Thijn <thijn@conduction.nl>
2025-08-12 11:23:32 +0200

    removed old detail pages

--- src/views/ac-beheer/index.js
+++ src/views/ac-beheer/index.js
@@ -14,34 +14,14 @@ const AcDashboard = loadable(() =>
 );
 
 // detail pages
-const AcBeheerApplicatiesDetails = loadable(() =>
-  import('@views/ac-beheer/domains/ac-applicaties/pages/ac-applicaties-details')
-);
-const AcBeheerDienstDetails = loadable(() =>
-  import('@views/ac-beheer/domains/ac-dienst/pages/ac-dienst-details')
-);
 const AcBeheerVoorzieningenVersieDetails = loadable(() =>
   import(
     '@views/ac-beheer/domains/ac-voorzieningen-versie/pages/ac-voorzieningen-versie-details'
   )
 );
-const AcBeheerGebruikenDetails = loadable(() =>
-  import('@views/ac-beheer/domains/ac-gebruiken/pages/ac-gebruiken-details')
-);
 const AcBeheerOvereenkomstenDetails = loadable(() =>
   import(
     '@views/ac-beheer/domains/ac-overeenkomsten/pages/ac-overeenkomsten-details'
-  )
-);
-const AcBeheerOrganisatieDetails = loadable(() =>
-  import('@views/ac-beheer/domains/ac-organisatie/pages/ac-organisatie-details')
-);
-const AcBeheerKwetsbaarheidDetails = loadable(() =>
-  import('@views/ac-beheer/domains/ac-kwetsbaarheid/pages/ac-kwetsbaarheid-details')
-);
-const AcBeheerContactpersoonDetails = loadable(() =>
-  import(
-    '@views/ac-beheer/domains/ac-contactpersonen/pages/ac-contactpersonen-details'
   )
 );
 
@@ -49,12 +29,6 @@ export {
   AcDashboard,
   AcBeheerError,
   AcBeheerLoading,
-  AcBeheerDienstDetails,
-  AcBeheerApplicatiesDetails,
   AcBeheerVoorzieningenVersieDetails,
-  AcBeheerGebruikenDetails,
   AcBeheerOvereenkomstenDetails,
-  AcBeheerOrganisatieDetails,
-  AcBeheerKwetsbaarheidDetails,
-  AcBeheerContactpersoonDetails,
 };
