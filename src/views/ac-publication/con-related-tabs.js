7166f0d55f92c57398906f7f65d70749476b8461
Remko Huisman <43807324+remko48@users.noreply.github.com>
2025-10-03 14:37:32 +0200

    filter element

--- src/views/ac-publication/con-related-tabs.js
+++ src/views/ac-publication/con-related-tabs.js
@@ -127,8 +127,10 @@ const mergeAndDeduplicateItems = (uses = [], used = []) => {
   // Combine both arrays
   const allItems = [...uses, ...used];
 
-  // Remove duplicates based on item ID
-  return _.uniqBy(allItems, 'id');
+  // Remove duplicates based on item ID and filter out elements
+  return _.uniqBy(allItems, 'id').filter(
+    (item) => item['@self']?.schema?.slug !== 'element'
+  );
 };
 
 // Helper function to render tabs for related objects
