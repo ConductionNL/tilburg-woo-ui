b40fae9ee11e77bfa96b3a9d68650d7e617dad29
Thijn <thijn@conduction.nl>
2025-10-09 09:59:07 +0200

    (Deprecation) moderate - @babel/plugin-proposal-private-methods

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,7 +28,7 @@ module.exports = {
       plugins: [
         ['@babel/plugin-proposal-decorators', { legacy: true }],
         ['@babel/plugin-transform-class-properties', { loose: true }],
-        ['@babel/plugin-proposal-private-methods', { loose: true }],
+        ['@babel/plugin-transform-private-methods', { loose: true }],
         ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
       ],
     },
