9dd1a38cf92f1c15cfb56422980e0d7cd792afae
Thijn <thijn@conduction.nl>
2025-10-09 10:01:24 +0200

    (Deprecation) moderate - @babel/plugin-proposal-private-property-in-object

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,7 @@ module.exports = {
         ['@babel/plugin-proposal-decorators', { legacy: true }],
         ['@babel/plugin-transform-class-properties', { loose: true }],
         ['@babel/plugin-transform-private-methods', { loose: true }],
-        ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+        ['@babel/plugin-transform-private-property-in-object', { loose: true }],
       ],
     },
     ecmaFeatures: {
