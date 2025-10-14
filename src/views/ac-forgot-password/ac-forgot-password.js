6037db7837b10396084d28bc28fba7e2e95e1b57
Remko <remko@conduction.nl>
2025-10-14 09:40:04 +0200

    Updated Login and Password forgot screens

--- src/views/ac-forgot-password/ac-forgot-password.js
+++ src/views/ac-forgot-password/ac-forgot-password.js
@@ -2,23 +2,23 @@ import { useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { withStore } from '@stores';
 import { AcFormField } from '@molecules';
-import { Heading, Link } from '@utrecht/component-library-react/dist/css-module';
+import { Heading } from '@utrecht/component-library-react/dist/css-module';
+import { Link } from 'react-router-dom';
 import AcButton from '@molecules/ac-button/ac-button';
 import { AcFlex } from '@src/atoms';
 import { VISUALS } from '@src/constants';
 
 const AcForgotPassword = () => {
   const [email, setEmail] = useState('');
-
   const isValidEmail = (email) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
   };
 
   return (
-    <AcFlex className='ac-forgot-password-container'>
+    <div className='ac-login-container'>
       <AcFlex column className='ac-forgot-password-container-inner'>
         <AcFlex className='ac-forgot-password-heading'>
-          <Heading level={3}>Wachtwoord vergeten?</Heading>
+          <Heading level={1}>Wachtwoord vergeten?</Heading>
         </AcFlex>
 
         <AcFlex column spacing='sm'>
@@ -48,11 +48,13 @@ const AcForgotPassword = () => {
           </AcButton>
 
           <AcFlex justifyContent='center'>
-            <Link href='/login'>Terug naar aanmelden</Link>
+            <Link className='utrecht-link utrecht-link--html-a' to='/login'>
+              <VISUALS.ARROW_LEFT /> Terug naar aanmelden
+            </Link>
           </AcFlex>
         </AcFlex>
       </AcFlex>
-    </AcFlex>
+    </div>
   );
 };
 
