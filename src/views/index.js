da4fdf9733108f5ffbcb33f3044cb198d2d76c82
Remko <remko@conduction.nl>
2025-12-31 12:29:04 +0100

    Moved gebruik to new folder

--- src/views/index.js
+++ src/views/index.js
@@ -24,7 +24,7 @@ const AcLogin = loadable(() => import('@views/ac-login/ac-login'));
 const ConDirectory = loadable(() => import('@views/con-directory/con-directory'));
 const AcObjects = loadable(() => import('@views/ac-beheer/ac-objects'));
 const AcFormsGebruik = loadable(() =>
-  import('@views/ac-forms/ac-forms-gebruik/ac-forms-gebruik')
+  import('@views/ac-forms/ac-forms-gebruik/gebruik-application/ac-forms-gebruik')
 );
 const AcFormsProduct = loadable(() =>
   import('@views/ac-forms/ac-forms-product/ac-forms-product')
