355a966498b33a357855a527fdabb39b350842d2
Thijn <thijn@conduction.nl>
2026-02-19 13:31:23 +0100

    text changes

--- src/views/ac-beheer/core/components/ac-dashboard.js
+++ src/views/ac-beheer/core/components/ac-dashboard.js
@@ -197,59 +197,41 @@ const AcDashboard = ({ store }) => {
             {/* Welcome Section */}
             <div className='ac-register-review__section'>
               <div className='ac-register-review__header'>
-                <Heading level={4}>Welkom in de Softwarecatalogus</Heading>
+                <Heading level={4}>Welkom in uw Softwarecatalogus</Heading>
               </div>
-              <Separator className='ac-register-review-header__separator' />
+              <Separator />
+
+              <div>
+                <Paragraph>
+                  Via deze omgeving publiceert en beheert u uw aanbod voor gemeenten.
+                  Hier legt u vast:
+                </Paragraph>
+
+                <ul style={{ marginInlineStart: '1.2rem' }}>
+                  <li>welke applicaties en diensten u aanbiedt</li>
+                  <li>welke koppelingen beschikbaar zijn</li>
+                  <li>
+                    hoe uw oplossing aansluit op de GEMeentelijke Model Architectuur
+                    (GEMMA)
+                  </li>
+                  <li>
+                    dat uw applicatie beschikbaar is voor opname in het gemeentelijke
+                    applicatielandschap
+                  </li>
+                </ul>
+              </div>
 
               <Paragraph>
-                Dit is de centrale plek om producten, applicaties, diensten en
-                koppelingen te beheren. Door applicaties te koppelen aan
-                GEMMA-referentiecomponenten wordt uw applicatielandschap gemapped op
-                de GEMMA-referentiearchitectuur.
+                Wilt u een nieuwe applicatie, dienst of koppeling publiceren? Gebruik
+                dan de acties bovenaan deze pagina. Een overzicht van uw reeds
+                gepubliceerde applicaties, diensten en koppelingen vindt u via het
+                linkermenu.
               </Paragraph>
 
-              <div className='ac-register-review__field'>
-                <strong>Dienst registreren:</strong>
-                <span>leg vast welke diensten u bij een product afneemt.</span>
-              </div>
-
-              <div className='ac-register-review__field'>
-                <strong>Gebruik registreren:</strong>
-                <span>
-                  registreer hoe uw organisatie applicaties inzet in processen en
-                  werkstromen.
-                </span>
-              </div>
-
-              <div className='ac-register-review__field'>
-                <strong>Koppeling registreren:</strong>
-                <span>leg vast welke koppelingen uw organisatie gebruikt.</span>
-              </div>
-
-              <Paragraph className='ac-dashboard-link-container'>
-                Voor accountdetails gaat u naar
-                <Link
-                  className='ac-dashboard-link'
-                  href='/beheer/my-account'
-                  onClick={(e) => {
-                    e.preventDefault();
-                    navigate('/beheer/my-account');
-                  }}
-                >
-                  Mijn Account
-                </Link>
-                . Voor organisatiedetails gaat u naar
-                <Link
-                  className='ac-dashboard-link'
-                  href='/beheer/my-organisation'
-                  onClick={(e) => {
-                    e.preventDefault();
-                    navigate('/beheer/my-organisation');
-                  }}
-                >
-                  Mijn Organisatie
-                </Link>
-                .
+              <Paragraph>
+                Gemeenten gebruiken deze informatie bij het vergelijken, selecteren
+                en inkopen van applicaties. Zorg daarom dat uw gegevens volledig en
+                actueel zijn.
               </Paragraph>
             </div>
           </AcFlex>
